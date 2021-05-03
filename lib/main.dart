08dede30cab72065d49f354e42664a93fe1cdc22
dfatih <doenmezfatih@outlook.com>
2021-05-03 17:53:32 +0200

    Adding ListView Like Button Like Indicator Like Counter Card View, to display Likes post and author

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -25,11 +43,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPost(String text) {
     setState(() {
-      this.text = text;
+      posts.add(new Post(text, "Max"));
     });
   }
 
@@ -38,7 +56,10 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text("Hello")),
       body: Column(
-        children: <Widget>[TextInputWidget(this.changeText), Text(this.text)],
+        children: <Widget>[
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost),
+        ],
       ),
     );
   }
@@ -63,8 +84,9 @@ class _TextInputWidgetState extends State<TextInputWidget> {
   }
 
   void click() {
-    widget.callback(controller.text);
+    FocusScope.of(context).unfocus();
     controller.clear();
+    widget.callback(controller.text);
   }
 
   @override
@@ -78,6 +100,55 @@ class _TextInputWidgetState extends State<TextInputWidget> {
               icon: Icon(Icons.send),
               splashColor: Colors.orange,
               onPressed: this.click)),
+    );
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callBack) {
+    this.setState(() {
+      callBack();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: ListTile(
+                title: Text(post.body),
+                subtitle: Text(post.author),
+              )),
+              Row(
+                children: <Widget>[
+                  Container(
+                      child: Text(post.likes.toString(),
+                          style: TextStyle(fontSize: 20)),
+                      padding: EdgeInsets.fromLTRB(0, 0, 10, 0)),
+                  IconButton(
+                      icon: Icon(Icons.thumb_up),
+                      onPressed: () => this.like(post.likePost),
+                      color: post.userLiked ? Colors.orange : Colors.black)
+                ],
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
